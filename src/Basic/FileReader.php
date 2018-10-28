8965a54b6ab9c7160b9a20a931262a626356dd44
faryar <faryar76@hotmail.com>
2018-10-28 14:34:32 +0000

    Apply fixes from StyleCI

--- src/Basic/FileReader.php
+++ src/Basic/FileReader.php
@@ -1,81 +1,88 @@
-<?php 
+<?php
+
 namespace Faryar\Cdnjs\Basic;
 
-class FileReader 
+class FileReader
 {
     /**
-     * path of cdnjs_lib 
+     * path of cdnjs_lib.
      *
      * @var string
      */
     protected $path;
     /**
-     * $path file after read will put here
+     * $path file after read will put here.
      *
      * @var json_decode data
      */
     protected $file;
 
     /**
-     * constuctor
+     * constuctor.
      *
      * @param [type] $path
      */
-    function __construct($path=null)
+    public function __construct($path = null)
     {
-        $this->path=is_null($path) ?  storage_path('cdnjs_lib.json') : $path ;
+        $this->path = is_null($path) ? storage_path('cdnjs_lib.json') : $path;
     }
+
     /**
-     * read $path file if exists and create if not
+     * read $path file if exists and create if not.
      *
      * @param [type] $path
+     *
      * @return void
      */
-    public function open($path=Null)
+    public function open($path = null)
     {
         $this->fileExists($this->path) ? null : $this->create();
-        $this->file=json_decode(file_get_contents($this->path));
+        $this->file = json_decode(file_get_contents($this->path));
+
         return $this;
     }
+
     /**
-     * check for exists file in $path
+     * check for exists file in $path.
      *
      * @param [type] $path
+     *
      * @return void
      */
     public function fileExists($path)
     {
         return file_exists($path);
     }
+
     /**
-     * create new file 
+     * create new file.
      *
      * @param string $name
+     *
      * @return void
      */
-    public function create($name='cdnjs_lib.json')
+    public function create($name = 'cdnjs_lib.json')
     {
-       return touch(storage_path($name));
+        return touch(storage_path($name));
     }
+
     /**
-     * check record exists in $path
+     * check record exists in $path.
      *
      * @param string $name
+     *
      * @return void
      */
     public function exists(string $name)
     {
-        if(! is_null($this->file))
-        {
-            foreach($this->file as $key => $link)
-            {
-                if($key==$name)
-                {
+        if (!is_null($this->file)) {
+            foreach ($this->file as $key => $link) {
+                if ($key == $name) {
                     return $link;
                 }
             }
         }
+
         return false;
     }
-
 }
--- src/Cdnjs.php
+++ src/Cdnjs.php
@@ -1,84 +1,87 @@
-<?php 
-namespace Faryar\Cdnjs;
-use Faryar\Cdnjs\Basic\Library;
-use Faryar\Cdnjs\Basic\FileReader as File;
+<?php
 
-class Cdnjs 
+namespace Faryar\Cdnjs;
+
+use Faryar\Cdnjs\Basic\FileReader as File;
+use Faryar\Cdnjs\Basic\Library;
+
+class Cdnjs
 {
     /**
-     * instans of Library class 
+     * instans of Library class.
      *
      * @var object
      */
     protected $library;
     /**
-     * instans of FileReader class 
+     * instans of FileReader class.
      *
      * @var object
      */
     protected $file;
 
     /**
-     * construct
+     * construct.
      */
     public function __construct()
     {
-        $this->library=new Library();
-        $this->file=new File();
+        $this->library = new Library();
+        $this->file = new File();
     }
+
     /**
-     * requre directive to provider
+     * requre directive to provider.
      *
      * @return void
      */
     public function directive()
     {
-        require(__DIR__."/directive.php"); 
+        require __DIR__.'/directive.php';
     }
+
     /**
-     * handle genarate
+     * handle genarate.
      *
      * @param [type] $name
      * @param [type] $version
+     *
      * @return void
      */
-    public function generate($input,$version=null)
+    public function generate($input, $version = null)
     {
-        if(is_array($input))
-        {
-            $results="";
-            foreach($input as $item)
-            {
-                $results.=$this->get($item);
+        if (is_array($input)) {
+            $results = '';
+            foreach ($input as $item) {
+                $results .= $this->get($item);
             }
+
             return $results;
         }
-        return $this->get($input);
 
+        return $this->get($input);
     }
+
     /**
-     * get links from Library class 
+     * get links from Library class.
      *
      * @param string $name
+     *
      * @return void
      */
     public function get(string $name)
     {
-        $results="";
-        if($name=="")
-        {
-           return $this->library->notFound("' no name '");
+        $results = '';
+        if ($name == '') {
+            return $this->library->notFound("' no name '");
         }
-        if($link=$this->file->open()->exists($name))
-        {
-            $results=$link;
-        }elseif($this->library->findRealPackageName($name))
-        {
-            $results=$this->library->get($name);
+        if ($link = $this->file->open()->exists($name)) {
+            $results = $link;
+        } elseif ($this->library->findRealPackageName($name)) {
+            $results = $this->library->get($name);
+        } else {
+            $results = $this->library->notFound($name);
+        }
 
-        }else{
-            $results= $this->library->notFound($name);
-        }
         return $results;
     }
 }
